7a1c5a1db62facc93b0641cb293d62e18c8172d5
Andre gois <andre.s.gois3@gmail.com>
2021-11-13 12:06:41 -0300

    adding external CSS module

---
+++ app.module.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",  
+  },
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 40,
+    color: '#fff',
+  }
+});
+
